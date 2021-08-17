6c488f922307629815bc1059731fbf75e0a1f2df
rexfordkode <86994591+rexfordkode@users.noreply.github.com>
2021-08-17 18:03:28 +0100

    Initial commit

---
+++ celsius-to-fahrenheit.js
@@ -0,0 +1,8 @@
+/* celsius-to-fahrenheit.js */
+const { celsiusToFahrenheit } = require('./converters.js');
+ 
+const celsiusInput = process.argv[2]; 
+const fahrenheitValue = celsiusToFahrenheit(input);
+ 
+console.log(`${celsiusInput} degrees Celsius = ${fahrenheitValue} degrees Fahrenheit`);
+
